62e1821e8772af00152205d2a20b2e035cd3d357
forukotan <forukotan1@gmail.com>
2022-07-20 09:31:01 -0500

    operator1

---
+++ operator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace operators;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        var a = 17;
+        var b = 4;
+        var div = a / b;
+        var mod = a % b;
+        Console.WriteLine($"{a}/{b} is {div} with remainder {mod}");
+    }
+  
+    
+}
